4ef6bdcec59e696a3b940cd7055f131db232ab69
gitprzemek <przemyslaw.sz.home@gmail.com>
2018-11-20 19:36:13 +0100

    change js and css

--- script/main.js
+++ script/main.js
@@ -6,10 +6,24 @@ const schema = [{
     options: ["Ms", "Mr", "Mx", "Dr", "Madam", "Lord"]
   },
   {
+    fieldType: "SelectList",
+    name: "title",
+    multi: true,
+    label: "Title",
+    options: ["XX", "ss", "aa", "Dr", "Madam", "Lord"]
+  },
+  {
     fieldType: "TextInput",
     placeholder: "First Name",
     label: "First Name",
     name: "firstName"
+  },
+  {
+    fieldType: "NumberInput",
+    placeholder: "Age",
+    name: "age",
+    label: "Age",
+    minValue: 4
   },
   {
     fieldType: "TextInput",
@@ -25,52 +39,53 @@ const schema = [{
     minValue: 0
   }
 ]
-var textnode = document.createTextNode("Water"); 
+
 
 function loadObject() {
     const containerForm = `
     <form action="" class="form" id="form"></form>
     `
     document.body.innerHTML = containerForm;
-    
-    
     const formContainer = document.getElementById("form")
-    console.log(containerForm);
-    console.log(schema[0]);
     
     for(let i= 0; i < schema.length; i++){
         if(schema[i].fieldType == "SelectList"){
-        
+            
             let select = `
             <label>Title</label>
-            <select name="title">
-                <option value="${schema[i].options[0]}">${schema[i].options[0]}</option>
-                <option value="${schema[i].options[1]}">${schema[i].options[1]}</option>
-                <option value="${schema[i].options[2]}">${schema[i].options[2]}</option>
-                <option value="${schema[i].options[3]}">${schema[i].options[3]}</option>
-                <option value="${schema[i].options[4]}">${schema[i].options[4]}</option>
-                <option value="${schema[i].options[5]}">${schema[i].options[5]}</option>
-              </select>
+            <select class="select" name="${schema[i].name}">
+            </select>
             `
-            
             formContainer.innerHTML += select;
+
+            schema[i].options.forEach( item => {
+                let selectCont = document.getElementsByClassName("select")[i];
+                let option = `
+                <option value="${item}">${item}</option>
+                `
+                selectCont.innerHTML += option;
+            });
+
+            let selectCont = document.getElementsByClassName("select")[i];
+            if (schema[i].multi === true){
+                selectCont.setAttribute("multiple", "true");
+            }
+
         } else if (schema[i].fieldType == "TextInput"){
             let inputText = `
             <label for="">${schema[i].label}</label>
-            <input name="${schema[i].label}" type="${schema[i].label}" placeholder="${schema[i].label}">
+            <input name="${schema[i].name}" type="text" placeholder="${schema[i].placeholder}">
             `
             formContainer.innerHTML += inputText;
+
         }else if (schema[i].fieldType == "NumberInput"){
             let inputNum = `
             <label for="">${schema[i].label}</label>
-            <input name="${schema[i].label}" type="${schema[i].label}" placeholder="${schema[i].label}">
+            <input name="${schema[i].name}" type="number" placeholder="${schema[i].placeholder}" min="${schema[i].minValue}">
             `
             formContainer.innerHTML += inputNum;
         }
-        
-
     }
-    
          
 };
 loadObject();
